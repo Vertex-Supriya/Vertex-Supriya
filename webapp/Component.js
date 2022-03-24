0e2c6c4563fd2537270454967902c37d51784502
Vertex-Supriya <Supriya.Bansal@vertexone.net>
2022-03-24 19:17:00 +0530

    first comment

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("ZRBBPCHANGE.zrbbpchange.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
